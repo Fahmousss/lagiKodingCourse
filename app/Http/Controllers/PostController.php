5b92bb7c36aea3516373aee339ab8b4c2f060918
fahmousss <=>
2024-02-18 21:58:43 +0700

    Refactor PostController and HandleInertiaRequests***

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,32 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        $data = Post::all();
+        return Inertia::render('Posts/Index', ['data' => $data]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): Response
     {
-        //
+        return Inertia::render('Posts/Create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request): RedirectResponse
+  {
+        $validated = $request->validate([
+            'title' => ['required', 'max:50'],
+            'body' => ['required'],
+        ]);
+        $item = Post::create($validated);
+        return redirect(route('posts.index'))->with('success', 'Post created.');
     }
 
     /**
@@ -42,24 +51,36 @@ class PostController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Post $post)
+    public function edit(Post $post): Response
     {
-        //
+        return Inertia::render('Posts/Edit', [
+            'post' => [
+                'id' => $post->id,
+                'title' => $post->title,
+                'body' => $post->body,
+            ],
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post)
+    public function update(Request $request, Post $post): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'title' => ['required', 'max:50'],
+            'body' => ['required'],
+        ]);
+        $post->update($validated);
+        return redirect(route('posts.index'))->with('success', 'Post updated.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy(Post $post): RedirectResponse
     {
-        //
+        $post->delete();
+        return redirect(route('posts.index'))->with('success', 'Post deleted.');
     }
 }
